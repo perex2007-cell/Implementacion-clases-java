07a63d993b53b9e3598d940798e399e6c643a489
Alexander Perez Ortiz <perex2007@gmail.com>
2026-02-20 07:50:06 -0500

    main terminado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,5 +14,17 @@ public class Main {
         System.out.println("Precio: " + product1.getPrecio());
         System.out.println("Descripción: " + product1.getDescripcion());
         System.out.println();
+
+        
+        Product product2 = new Product(34, "Teclado", "Teclado mecánico con retroiluminación", 45000);
+        System.out.println("Producto 2:");
+        System.out.println("Id: " + product2.getId());
+        System.out.println("Nombre: " + product2.getNombre());
+        System.out.println("Precio: " + product2.getPrecio());
+        System.out.println("Descripción: " + product2.getDescripcion());
+        System.out.println();
+
+        System.out.println("Product1: " + product1);
+        System.out.println("Product2: " + product2);
     }
 }
