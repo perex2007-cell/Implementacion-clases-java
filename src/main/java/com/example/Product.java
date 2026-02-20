636b2d3f321aa0dbeea926218eba708fa691290b
Alexander Perez Ortiz <perex2007@gmail.com>
2026-02-20 07:50:50 -0500

    mitad del product

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -2,4 +2,34 @@ package com.example;
 
 public class Product {
     
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public Product() {
+        this.id = 0;
+        this.nombre = "";
+        this.descripcion = "";
+        this.precio = 0.0;
+    }
+
+    public Product(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
